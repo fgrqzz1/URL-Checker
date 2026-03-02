c4919a2aebcda1bdb405b96d604084d5756c2359
Grachev Yaroslav <grachev.yaroslav496@gmail.com>
2026-03-02 11:49:27 +0300

    add: checker for one url request

--- internal/checker/checker.go
+++ internal/checker/checker.go
@@ -1,1 +1,29 @@
 package checker
+
+import (
+	"net/http"
+	"time"
+	"url-checker/internal/models"
+)
+
+func CheckerURL(url string) models.CheckResult {
+	timeStart := time.Now()
+
+	resp, err := http.Get(url)
+	latency := time.Since(timeStart)
+
+	if err != nil {
+		return models.CheckResult{
+			URL:     url,
+			Error:   err,
+			Latency: latency,
+		}
+	}
+	defer resp.Body.Close()
+
+	return models.CheckResult{
+		URL:     url,
+		Status:  resp.StatusCode,
+		Latency: latency,
+	}
+}
